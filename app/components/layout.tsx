4a8c243d6b47e67cf6ef89a4d40df3d614d51191
Max Allan-Smith <Maxibon_1195@hotmail.com>
2024-04-01 19:19:19 +0100

    Couldn't understand TypeScript, so starting again with JavaScript

--- app/components/layout.tsx
+++
@@ -1,5 +0,0 @@
-import React, { Children } from "react";
-
-export function Layout({ children }: { children: React.ReactNode }) {
-  return <div className=" h-screen w-full bg-slate-500 ">{children}</div>;
-}
